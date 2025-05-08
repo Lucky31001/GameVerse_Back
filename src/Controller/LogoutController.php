7846b599784b1f405177ad8d35fb14259b23595d
Lucky31001 <kilian.dumaire@gmail.com>
2025-05-08 23:51:03 +0200

    feat: we can fully log out

---
+++ src/Controller/LogoutController.php
@@ -0,0 +1,38 @@
+<?php
+
+// src/Controller/LogoutController.php
+
+namespace App\Controller;
+
+use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LogoutController extends AbstractController
+{
+    public function __construct(
+        private RefreshTokenManagerInterface $refreshTokenManager,
+    ) {
+    }
+
+    public function __invoke(Security $security): JsonResponse
+    {
+
+        $user = $security->getUser();
+
+        if (!$user) {
+            return new JsonResponse(['message' => 'Already logged out'], 200);
+        }
+
+        $refreshToken = $this->refreshTokenManager->getLastFromUsername($user->getUserIdentifier());
+
+        if ($refreshToken) {
+            $this->refreshTokenManager->delete($refreshToken);
+        }
+
+        return new JsonResponse(['message' => 'Successfully logged out']);
+    }
+}
